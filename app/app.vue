1f1948d338f88ef69db7f992cd94e2135b3f4eea
Jakub Frelik <git@jfrelik.dev>
2025-03-25 10:42:33 +0100

    chore: add prettier, lint-staged, husky

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,12 @@
+<script lang="ts" setup>
+	const msg = 'Hello Vue 3 + TypeScript + Vite';
+	const aaaaa = () => {
+		return 12312;
+	};
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<div>
+		<NuxtWelcome />
+	</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
 
-  future: {
-    compatibilityVersion: 4
-  },
+	future: {
+		compatibilityVersion: 4,
+	},
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
-})
+	modules: [
+		'@nuxt/eslint',
+		'@nuxt/fonts',
+		'@nuxt/icon',
+		'@nuxt/image',
+		'@nuxt/test-utils',
+		'@nuxt/ui',
+	],
+});
