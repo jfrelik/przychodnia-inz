ce70850e57720095b48df1620bf9e9792e6f9203
Jakub Frelik <git@jfrelik.dev>
2025-08-04 20:50:09 +0200

    style(footer): minimal design

--- app/components/PageFooter.vue
+++ app/components/PageFooter.vue
@@ -1,23 +1,10 @@
 <script lang="ts" setup></script>
 
 <template>
-	<div
-		class="flex flex-row items-center bg-neutral-800 pt-8 pr-8 pb-8 pl-8 text-gray-400"
-	>
-		<div class="flex flex-row items-center">
-			<img src="/favicon.ico" />
-			<div class="ml-3 flex flex-col align-baseline">
-				<h1 class="text-2xl font-bold">NazwaPrzychodni</h1>
-				<p>Motto przychodni</p>
-			</div>
-		</div>
-		<div class="ml-auto max-w-1/3 flex-wrap text-xs">
-			<p>
-				Projekt wykonany w ramach pracy inżynierskiej przez Jakuba Frelika i
-				Macieja Cynkara
-			</p>
-		</div>
+	<div class="flex flex-row items-center bg-black px-8 py-4 text-gray-400">
+		<p class="text-sm font-light">
+			Projekt wykonany w ramach pracy inżynierskiej przez Jakuba Frelika i
+			Macieja Cynkara
+		</p>
 	</div>
 </template>
-
-<style></style>
