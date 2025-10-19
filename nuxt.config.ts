ec7922a495889338f9fd2167d7515b29e7eb9f7a
Jakub Frelik <git@jfrelik.dev>
2025-10-19 21:02:17 +0200

    chore: bump compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-11-01',
+	compatibilityDate: '2025-10-10',
 	devtools: { enabled: true },
 	css: ['~/assets/css/main.css'],
 
