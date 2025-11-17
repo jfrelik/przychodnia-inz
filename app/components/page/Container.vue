cbe47e21ffaa1844bb2d6e910951f10f57526605
Jakub Frelik <git@jfrelik.dev>
2025-11-17 16:32:59 +0100

    chore: add classes to pagecontainer

--- app/components/page/Container.vue
+++ app/components/page/Container.vue
@@ -1,5 +1,7 @@
 <template>
-	<UContainer class="flex flex-col gap-y-6">
+	<UContainer
+		class="mx-auto flex min-h-0 w-full max-w-6xl flex-1 flex-col gap-6 px-6 py-8"
+	>
 		<slot />
 	</UContainer>
 </template>
