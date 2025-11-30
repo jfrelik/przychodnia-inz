bd0c2ab5f9e7545fd83957ab26f7b7d49ce4ccec
Jakub Frelik <git@jfrelik.dev>
2025-11-30 19:54:09 +0100

    feat: rework auth middleware

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -2,43 +2,20 @@ import { authClient } from '~~/lib/auth-client';
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
 	const { data: session } = await authClient.useSession(useFetch);
+	const role = session.value?.user?.role;
+	const publicPages = ['/', '/login', '/register'];
 
-	const isDev = import.meta.dev;
-	const publicPaths = [
-		'/login',
-		'/register',
-		'/commute',
-		'/contactUs',
-		'/openapi.yaml',
-		'/openapi.json',
-		'/_openapi.json',
-	];
+	// Allow public pages
+	if (publicPages.includes(to.path)) return;
 
-	const isPublic =
-		publicPaths.includes(to.path) || (isDev && to.path.startsWith('/docs'));
+	if (!session.value?.user) return navigateTo('/login', { replace: true });
 
-	// if (!session.value && !isPublic) {
-	// 	return navigateTo('/register');
-	// }
-
-	if (to.path.startsWith('/admin') && session.value?.user.role !== 'admin') {
-		console.log(
-			`User ${session.value?.user.email} attemped to access route ${to.path} without proper roles required`
-		);
-		return false;
-	}
-
-	if (to.path.startsWith('/user') && session.value?.user.role !== 'user') {
-		console.log(
-			`User ${session.value?.user.email} attemped to access route ${to.path} without proper roles required`
-		);
-		return false;
-	}
-
-	if (to.path.startsWith('/doctor') && session.value?.user.role !== 'doctor') {
-		console.log(
-			`User ${session.value?.user.email} attemped to access route ${to.path} without proper roles required`
-		);
-		return false;
-	}
+	if (to.path.startsWith('/admin/') && role !== 'admin')
+		return navigateTo('/', { replace: true });
+	if (to.path.startsWith('/doctor/') && role !== 'doctor')
+		return navigateTo('/', { replace: true });
+	if (to.path.startsWith('/user/') && role !== 'user')
+		return navigateTo('/', { replace: true });
+	if (to.path.startsWith('/receptionist/') && role !== 'receptionist')
+		return navigateTo('/', { replace: true });
 });
