76e1197716cc815c5303c7424fb883c19452211d
Jakub Frelik <git@jfrelik.dev>
2026-01-22 17:22:18 +0100

    feat: rework auth middleware

--- app/layouts/admin.vue
+++ app/layouts/admin.vue
@@ -10,19 +10,14 @@
 
 	const handleSignout = async () => {
 		try {
-			await authClient.signOut({
-				fetchOptions: {
-					onSuccess: () => {
-						toast.add({
-							title: 'Wylogowano',
-							description: 'Proces wylogowywania powiódł się',
-							color: 'success',
-							icon: 'lucide:check',
-						});
-						navigateTo('/');
-					},
-				},
+			await authClient.signOut();
+			toast.add({
+				title: 'Wylogowano',
+				description: 'Proces wylogowywania powiódł się',
+				color: 'success',
+				icon: 'lucide:check',
 			});
+			navigateTo('/login?logout=true', { replace: true });
 		} catch (error) {
 			const message = error instanceof Error ? error.message : String(error);
 			toast.add({
